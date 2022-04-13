be244c0c5e1cf2c339c4b378a9743245f0a054a3
Andreas Jacobs <ajaco@xs4all.nl>
2022-04-13 09:22:45 +0200

    Initial Commit

---
+++ testttt/testtttApp.swift
@@ -0,0 +1,17 @@
+//
+//  testtttApp.swift
+//  testttt
+//
+//  Created by CDust Creative Engineering on 13/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct testtttApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
